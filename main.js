dad56cbf18d3809e2a68ea213362f81ef7895bca
Roly Fentanes <roly426@gmail.com>
2013-09-06 05:32:34 -0400

    compile on client side

---
+++ main.js
@@ -0,0 +1,46 @@
+/*global dust, pokemon */
+var tmpl =  '' +
+  '  {#rows}' +
+  '  <tr>' +
+  '    {#pokemon}' +
+  '    <td{#weeknum} class="weeknum-{.}"{/weeknum}>' +
+  '      <div class="number">{number}</div>' +
+  '      <div class="name">{name|s}</div>' +
+  '      <img src="{url}" alt="{number}" />' +
+  '      {#move}<div class="move">{.}</div>{/move}' +
+  '    </td>' +
+  '    {/pokemon}' +
+  '  </tr>' +
+  '  {/rows}' +
+  '</table>';
+
+
+// Organize data for template.
+var data = { rows: [] };
+for (var i = -1; i < 10; i++) {
+  var row = [];
+  for (var j = 0; j < 10; j++) {
+    var number = i === -1 ? '' + j : '' + i + j;
+    var p = pokemon[number];
+    row.push({
+      number: number,
+      name: p.name,
+      move: p.move,
+      weeknum: p.weeknum,
+      url: 'img/' + number + '.png',
+    });
+  }
+  data.rows.push({ pokemon: row });
+}
+
+// Compile template.
+var compiled = dust.compileFn(tmpl, 'body');
+dust.loadSource(compiled);
+
+// Render template.
+dust.render('body', data, function(err, output) {
+  if (err) throw err;
+  $(function() {
+    $('body').html(output);
+  });
+});
---
+++ pokemon.js
@@ -0,0 +1,539 @@
+var pokemon = {
+  "0": {
+    "name": "egg",
+    "weeknum": 0
+  },
+  "1": {
+    "name": "pokeflute",
+    "weeknum": 1
+  },
+  "2": {
+    "name": "crane",
+    "weeknum": 2
+  },
+  "3": {
+    "name": "cat",
+    "weeknum": 3
+  },
+  "4": {
+    "name": "sailboat",
+    "weeknum": 4
+  },
+  "5": {
+    "name": "fisherman",
+    "weeknum": 5
+  },
+  "6": {
+    "name": "bob-omb",
+    "weeknum": 6
+  },
+  "7": {
+    "name": "boomerang"
+  },
+  "8": {
+    "name": "snowman"
+  },
+  "9": {
+    "name": "balloon"
+  },
+  "00": {
+    "name": "missingno",
+    "move": "multiply master balls",
+    "weeknum": 0
+  },
+  "01": {
+    "name": "bulbasaur",
+    "move": "vine whip",
+    "weeknum": 1
+  },
+  "02": {
+    "name": "ivysaur",
+    "move": "razor leaf",
+    "weeknum": 2
+  },
+  "03": {
+    "name": "venusaur",
+    "move": "solar beam",
+    "weeknum": 3
+  },
+  "04": {
+    "name": "charmander",
+    "move": "ember",
+    "weeknum": 5
+  },
+  "05": {
+    "name": "charmeleon",
+    "move": "rage",
+    "weeknum": 6
+  },
+  "06": {
+    "name": "charizard",
+    "move": "seismic toss",
+    "weeknum": 0
+  },
+  "07": {
+    "name": "squirtle",
+    "move": "water gun",
+    "weeknum": 1
+  },
+  "08": {
+    "name": "wartortle",
+    "move": "bubblebeam",
+    "weeknum": 3
+  },
+  "09": {
+    "name": "blastoise",
+    "move": "hydro pump",
+    "weeknum": 4
+  },
+  "10": {
+    "name": "caterpie",
+    "move": "string shot",
+    "weeknum": 5
+  },
+  "11": {
+    "name": "metapod",
+    "move": "harden (shiny)",
+    "weeknum": 6
+  },
+  "12": {
+    "name": "butterfree",
+    "move": "sleep power",
+    "weeknum": 1
+  },
+  "13": {
+    "name": "weedle",
+    "move": "poison sting",
+    "weeknum": 2
+  },
+  "14": {
+    "name": "kakuna",
+    "move": "harden (hanging on branch)",
+    "weeknum": 3
+  },
+  "15": {
+    "name": "beedrill",
+    "move": "fury attack",
+    "weeknum": 4
+  },
+  "16": {
+    "name": "pidgey",
+    "move": "sand attack",
+    "weeknum": 6
+  },
+  "17": {
+    "name": "pidgeotto",
+    "move": "gust",
+    "weeknum": 0
+  },
+  "18": {
+    "name": "pidgeot",
+    "move": "wing attack",
+    "weeknum": 1
+  },
+  "19": {
+    "name": "rattata",
+    "move": "tackle",
+    "weeknum": 2
+  },
+  "20": {
+    "name": "raticate",
+    "move": "super fang",
+    "weeknum": 4
+  },
+  "21": {
+    "name": "spearow",
+    "move": "peck",
+    "weeknum": 5
+  },
+  "22": {
+    "name": "fearow",
+    "move": "drill peck",
+    "weeknum": 6
+  },
+  "23": {
+    "name": "ekans",
+    "move": "wrap",
+    "weeknum": 0
+  },
+  "24": {
+    "name": "arbok",
+    "move": "glare",
+    "weeknum": 2
+  },
+  "25": {
+    "name": "pikachu",
+    "move": "thunder",
+    "weeknum": 3
+  },
+  "26": {
+    "name": "raichu",
+    "move": "thunderbolt",
+    "weeknum": 4
+  },
+  "27": {
+    "name": "sandshrew",
+    "move": "dig",
+    "weeknum": 5
+  },
+  "28": {
+    "name": "sandslash",
+    "move": "slash",
+    "weeknum": 0
+  },
+  "29": {
+    "name": "nidoran&#9792;",
+    "move": "screech",
+    "weeknum": 1
+  },
+  "30": {
+    "name": "nidorina",
+    "move": "ice beam",
+    "weeknum": 2
+  },
+  "31": {
+    "name": "nidoqueen",
+    "move": "substitute",
+    "weeknum": 3
+  },
+  "32": {
+    "name": "nidoran&#9794;",
+    "move": "double kick",
+    "weeknum": 5
+  },
+  "33": {
+    "name": "nidorino",
+    "move": "toxic spikes",
+    "weeknum": 6
+  },
+  "34": {
+    "name": "nidoking",
+    "move": "hyper beam",
+    "weeknum": 0
+  },
+  "35": {
+    "name": "clefairy",
+    "move": "metronome",
+    "weeknum": 1
+  },
+  "36": {
+    "name": "clefable",
+    "move": "body slam",
+    "weeknum": 3
+  },
+  "37": {
+    "name": "vulpix",
+    "move": "will-o-wisp",
+    "weeknum": 4
+  },
+  "38": {
+    "name": "ninetails",
+    "move": "fire spin",
+    "weeknum": 5
+  },
+  "39": {
+    "name": "jigglypuff",
+    "move": "sing",
+    "weeknum": 6
+  },
+  "40": {
+    "name": "wigglytuff",
+    "move": "double slap",
+    "weeknum": 1
+  },
+  "41": {
+    "name": "zubat",
+    "move": "supersonic",
+    "weeknum": 2
+  },
+  "42": {
+    "name": "golbat",
+    "move": "bite",
+    "weeknum": 3
+  },
+  "43": {
+    "name": "oddish",
+    "move": "sweet scent",
+    "weeknum": 4
+  },
+  "44": {
+    "name": "gloom",
+    "move": "protect",
+    "weeknum": 6
+  },
+  "45": {
+    "name": "vileplume",
+    "move": "petal dance",
+    "weeknum": 0
+  },
+  "46": {
+    "name": "paras",
+    "move": "stun spore",
+    "weeknum": 1
+  },
+  "47": {
+    "name": "parasect",
+    "move": "leech life",
+    "weeknum": 2
+  },
+  "48": {
+    "name": "venonat",
+    "move": "foresight",
+    "weeknum": 4
+  },
+  "49": {
+    "name": "venomoth",
+    "move": "confusion",
+    "weeknum": 5
+  },
+  "50": {
+    "name": "digglet",
+    "move": "earthquake",
+    "weeknum": 6
+  },
+  "51": {
+    "name": "dugtrio",
+    "move": "mud slap",
+    "weeknum": 0
+  },
+  "52": {
+    "name": "meowth",
+    "move": "scratch",
+    "weeknum": 2
+  },
+  "53": {
+    "name": "persian",
+    "move": "payday",
+    "weeknum": 3
+  },
+  "54": {
+    "name": "psyduck",
+    "move": "light screen",
+    "weeknum": 4
+  },
+  "55": {
+    "name": "golduck",
+    "move": "water pulse",
+    "weeknum": 5
+  },
+  "56": {
+    "name": "mankey",
+    "move": "swagger",
+    "weeknum": 0
+  },
+  "57": {
+    "name": "primeape",
+    "move": "trash",
+    "weeknum": 1
+  },
+  "58": {
+    "name": "growlithe",
+    "move": "growl",
+    "weeknum": 2
+  },
+  "59": {
+    "name": "arcanine",
+    "move": "extreme speed",
+    "weeknum": 3
+  },
+  "60": {
+    "name": "poliwag",
+    "move": "wake-up slap",
+    "weeknum": 5
+  },
+  "61": {
+    "name": "poliwhirl",
+    "move": "thief",
+    "weeknum": 6
+  },
+  "62": {
+    "name": "poliwrath",
+    "move": "blizzard",
+    "weeknum": 0
+  },
+  "63": {
+    "name": "abra",
+    "move": "teleport",
+    "weeknum": 1
+  },
+  "64": {
+    "name": "kadabra",
+    "move": "psybeam",
+    "weeknum": 3
+  },
+  "65": {
+    "name": "alakazam",
+    "move": "psychic",
+    "weeknum": 4
+  },
+  "66": {
+    "name": "machop",
+    "move": "karate chop",
+    "weeknum": 5
+  },
+  "67": {
+    "name": "machoke",
+    "move": "rock tomb",
+    "weeknum": 6
+  },
+  "68": {
+    "name": "machamp",
+    "move": "frustration",
+    "weeknum": 1
+  },
+  "69": {
+    "name": "bellsprout",
+    "move": "swords dance",
+    "weeknum": 2
+  },
+  "70": {
+    "name": "weepinbell",
+    "move": "energy ball",
+    "weeknum": 3
+  },
+  "71": {
+    "name": "vitctreebel",
+    "move": "giga drain",
+    "weeknum": 4
+  },
+  "72": {
+    "name": "tentacool",
+    "move": "scald",
+    "weeknum": 6
+  },
+  "73": {
+    "name": "tentacruel",
+    "move": "constrict",
+    "weeknum": 0
+  },
+  "74": {
+    "name": "geodude",
+    "move": "flamethrower",
+    "weeknum": 1
+  },
+  "75": {
+    "name": "graveler",
+    "move": "explosion",
+    "weeknum": 2
+  },
+  "76": {
+    "name": "golem",
+    "move": "rollout",
+    "weeknum": 4
+  },
+  "77": {
+    "name": "ponyta",
+    "move": "return",
+    "weeknum": 5
+  },
+  "78": {
+    "name": "rapidash",
+    "move": "flame charge",
+    "weeknum": 6
+  },
+  "79": {
+    "name": "slowpoke",
+    "move": "yawn",
+    "weeknum": 0
+  },
+  "80": {
+    "name": "slowbro",
+    "move": "headbutt",
+    "weeknum": 2
+  },
+  "81": {
+    "name": "magnemite",
+    "move": "swift",
+    "weeknum": 3
+  },
+  "82": {
+    "name": "magneton",
+    "move": "thunder wave",
+    "weeknum": 4
+  },
+  "83": {
+    "name": "farfetch'd",
+    "move": "cut",
+    "weeknum": 5
+  },
+  "84": {
+    "name": "doduo",
+    "move": "aerial ace",
+    "weeknum": 0
+  },
+  "85": {
+    "name": "dodrio",
+    "move": "fly",
+    "weeknum": 1
+  },
+  "86": {
+    "name": "seel",
+    "move": "tail whip",
+    "weeknum": 2
+  },
+  "87": {
+    "name": "dewgong",
+    "move": "hail",
+    "weeknum": 3
+  },
+  "88": {
+    "name": "grimer",
+    "move": "poison gas",
+    "weeknum": 5
+  },
+  "89": {
+    "name": "muk",
+    "move": "toxic",
+    "weeknum": 6
+  },
+  "90": {
+    "name": "shellder",
+    "move": "payback",
+    "weeknum": 0
+  },
+  "91": {
+    "name": "cloyster",
+    "move": "clamp",
+    "weeknum": 1
+  },
+  "92": {
+    "name": "gastly",
+    "move": "lick",
+    "weeknum": 3
+  },
+  "93": {
+    "name": "haunter",
+    "move": "fling",
+    "weeknum": 4
+  },
+  "94": {
+    "name": "gengar",
+    "move": "confuse ray",
+    "weeknum": 5
+  },
+  "95": {
+    "name": "onyx",
+    "move": "rock throw",
+    "weeknum": 6
+  },
+  "96": {
+    "name": "drowzee",
+    "move": "low sweep",
+    "weeknum": 1
+  },
+  "97": {
+    "name": "hypno",
+    "move": "hypnosis",
+    "weeknum": 2
+  },
+  "98": {
+    "name": "krabby",
+    "move": "x-scissor",
+    "weeknum": 3
+  },
+  "99": {
+    "name": "kingler",
+    "move": "guillotine",
+    "weeknum": 4
+  }
+};
